7b57a53c7ec5e45c73b9cd1e92e4529507f5ba3e
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-06-14 00:41:48 +0900

    てすと

---
+++ api/webhook.js
@@ -0,0 +1,33 @@
+import * as line from '@line/bot-sdk';
+
+const config = {
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+
+export default async function handler(req, res) {
+  if (req.method !== 'POST') {
+    res.status(405).send('Method Not Allowed');
+    return;
+  }
+
+  const middleware = line.middleware(config);
+  middleware(req, res, async () => {
+    const events = req.body.events;
+    const results = await Promise.all(events.map(handleEvent));
+    res.status(200).json(results);
+  });
+}
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return null;
+  }
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: `「${event.message.text}」ですね`,
+  });
+}

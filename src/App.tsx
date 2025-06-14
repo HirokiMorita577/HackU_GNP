b95579714ce4b398473f9f3869ed58e38d4489b6
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-06-14 09:57:19 +0900

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    liff.init({ liffId: import.meta.env.VITE_LIFF_ID }).then(async () => {
+      if (!liff.isLoggedIn()) {
+        liff.login();
+      } else {
+        const userProfile = await liff.getProfile();
+        console.log(userProfile);
+        setProfile(userProfile);
+      }
+    });
+  }, []);
 
   return (
     <>

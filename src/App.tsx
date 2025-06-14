2ec49406c9f15885c3a08bae3c11a3e697347b8a
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-06-14 11:13:05 +0900

    dw

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   const [count, setCount] = useState(0)
 
 useEffect(() => {
-  liff.init({ liffId: "あなたのLIFF ID" })
+  liff.init({ liffId: "2007570642-6BxVDbdl" })
     .then(async () => {
       console.log("LIFF初期化成功");
       if (!liff.isLoggedIn()) {

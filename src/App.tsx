cd25218838f4bde0b19d4559c2d44d006bc42bda
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-06-14 10:00:53 +0900

    wdwd

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import liff from '@line/liff';
 
 function App() {
   const [count, setCount] = useState(0)

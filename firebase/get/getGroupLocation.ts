189e0f5a0a30ae1b6d408ca6e55a3762b631a092
tmkttmkt <24rd123@ms.dendai.ac.jp>
2025-06-16 01:14:33 +0900

    お願い

--- firebase/get/getGroupLocation.ts
+++ firebase/get/getGroupLocation.ts
@@ -8,20 +8,17 @@ import database from "../firebaseConfig.js";
  */
 export async function getGroupLocations(groupId: string) {
   // グループメンバー一覧を取得
-  const membersRef = ref(database, `groups/${groupId}/members`);
+  const membersRef = ref(database, `groups/${groupId}/joiningMembers`);
   const membersSnap = await get(membersRef);
   if (!membersSnap.exists()) return [];
   const membersObj = membersSnap.val();
   const userIds = Object.keys(membersObj);
 
-  // 各ユーザーの位置情報を取得
+  // 各ユーザーの情報を取得
   const results = await Promise.all(userIds.map(async (userId) => {
-    const locRef = ref(database, `users/${userId}/locations`);
+    const locRef = ref(database, `users/${userId}`);
     const locSnap = await get(locRef);
-    return {
-      userId,
-      location: locSnap.exists() ? locSnap.val() : null
-    };
+    return locSnap
   }));
   return results;
 }

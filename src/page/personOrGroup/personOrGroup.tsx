429e49593900c5bfdc3c68704f280af30799195e
TMKT <126658967+tmkttmkt@users.noreply.github.com>
2025-06-16 14:45:09 +0900

    wasure

--- src/page/personOrGroup/personOrGroup.tsx
+++ src/page/personOrGroup/personOrGroup.tsx
@@ -1,5 +1,5 @@
 // src/components/PersonOrGroup.tsx
-import React, { use } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom'; // useNavigateフックをインポート
 import './personOrGroup.css';
 import { useAtom } from 'jotai';
